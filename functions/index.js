1ecc380f22d74bbd7090d5483b32f4b2301fd647
hananhanafi <hananhanafi@mail.ugm.ac.id>
2020-04-26 00:25:22 +0700

    Create Article API

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,16 @@
 const functions = require('firebase-functions');
+const app = require('express')();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+
+const {
+    getAllAticles, createArticle, updateArticle, deleteArticle, showArticle
+} = require('./APIs/articles');
+
+app.get('/articles', getAllAticles);
+app.post('/article' , createArticle);
+app.put('/article/:articleId' , updateArticle);
+app.delete('/article/:articleId' , deleteArticle);
+app.get('/article/:articleId', showArticle);
+
+exports.api = functions.https.onRequest(app);
+
