1831bb502b9429e999f70d642ccc0be7ddc5da4e
hananhanafi <hananhanafi@mail.ugm.ac.id>
2020-05-01 01:15:22 +0700

    create blog feature

--- view/src/pages/Blog.js
+++ view/src/pages/Blog.js
@@ -1,11 +1,65 @@
 import React,{ Component } from "react";
+import axios from 'axios';
+import { withRouter } from "react-router-dom";
 
 class Blog extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            articles:'',
+            load:true,
+        }
+    }
+
+    componentWillMount = () =>{
+        axios.get('/articles')
+        .then((response)=>{
+            this.setState({
+                articles : Array.from(response.data),
+                load:false
+
+            })
+        })
+    }
+
+    showHandle(data){
+        let articleId = data.data.articleId
+        console.log(data.data.articleId)
+        this.props.history.push(`/blog/${articleId}`)
+    }
+
     render(){
 
         return (
-        <div className="App-header">
-            <h1>Blog</h1>
+        <div>
+            <div className="container text-black">
+                <div className="row">
+                    <div className="col-12 my-5">
+                        <h1>Blog</h1>
+
+                    </div>
+                </div>
+                <div className="row mb-5">
+                    {Object.keys(this.state.articles).map((key)=>{
+                    {var data = this.state.articles[key]}
+                    return (
+                        <div className="col-12 mb-2">
+                            <div class="card w-100">
+                                <div class="card-body text-left">
+                                    <h5 class="card-title">{data.title}</h5>
+                                    <small>Created at : {data.createdAt}</small>
+                                    <button onClick={()=>this.showHandle({data})} class="btn btn-primary float-right">Baca</button>
+                                </div>
+                            </div>
+                        </div>
+                        )
+                    })}
+                    
+                    
+                </div>
+            </div>
 
 
         </div>
@@ -13,4 +67,4 @@ class Blog extends Component{
     }
 }
 
-export default Blog;
+export default withRouter(Blog);
--- view/src/pages/BlogDetail.js
+++ view/src/pages/BlogDetail.js
@@ -1,11 +1,58 @@
 import React,{ Component } from "react";
+import axios from 'axios';
+import { withRouter } from "react-router-dom";
 
 class BlogDetail extends Component{
+    constructor(props){
+        super(props);
+
+        this.state = {
+            article:'',
+            load:true,
+        }
+    }
+
+
+    componentWillMount(){
+        const {articleId} = this.props.match.params
+        console.log("detail",articleId);
+
+        axios.get(`/article/${articleId}`)
+        .then((response)=>{
+            this.setState({
+                article: response.data
+            })
+            console.log(response.data);
+        })
+    }
     render(){
 
         return (
-        <div className="App-header">
-
+        <div>
+            <div className="container">
+                <div className="row my-5">
+                    <div className="col text-left">
+                        <a href="/blog">Blog</a> > {this.state.article.title}
+                    </div>
+                </div>
+                <div className="row mb-5">
+                    <div className="col-12">
+                    <div class="card w-100 text-black p-5">
+                        <h1 class="card-title">{this.state.article.title}</h1>
+                    <div class="card-body text-left">
+                        <h6 class="card-subtitle mb-2 text-muted">
+                            author : {this.state.article.username}
+                        <br/>
+                            created at : {this.state.article.createdAt}</h6>
+                        <p class="card-text"><div
+                        dangerouslySetInnerHTML={{
+                            __html: this.state.article.body
+                        }}></div></p>
+                    </div>
+                    </div>
+                    </div>
+                </div>
+            </div>
 
 
         </div>
@@ -13,4 +60,4 @@ class BlogDetail extends Component{
     }
 }
 
-export default BlogDetail;
+export default withRouter(BlogDetail);
