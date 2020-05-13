8a265169c5c5341f8e9f839d1e417754337d4015
hananhanafi <hananhanafi@mail.ugm.ac.id>
2020-05-13 07:42:33 +0700

    responsive dashboard

--- view/src/pages/admin/Dashboard.js
+++ view/src/pages/admin/Dashboard.js
@@ -62,7 +62,7 @@ class Dashboard extends Component{
             <div id="wrapper">
 
                 {/* <!-- Sidebar --> */}
-                <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion text-white" id="accordionSidebar">
+                <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion text-white d-none d-md-block" id="accordionSidebar">
 
                     {/* <!-- Sidebar - Brand --> */}
                     <a class="sidebar-brand d-flex align-items-center justify-content-center">
@@ -106,35 +106,12 @@ class Dashboard extends Component{
 
                         {/* <!-- Topbar Navbar --> */}
                         <ul class="navbar-nav ml-auto">
-
-                            {/* <!-- Nav Item - Search Dropdown (Visible Only XS) --> */}
-                            <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            {/* <!-- Dropdown - Messages --> */}
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                <div class="input-group">
-                                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2"/>
-                                    <div class="input-group-append">
-                                    <button class="btn btn-primary" type="button">
-                                        <i class="fas fa-search fa-sm"></i>
-                                    </button>
-                                    </div>
-                                </div>
-                                </form>
-                            </div>
-                            </li>
-
-                        
-
                             <div class="topbar-divider d-none d-sm-block"></div>
 
                             {/* <!-- Nav Item - User Information --> */}
                             <li class="nav-item dropdown no-arrow">
                             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <span class="mr-2 d-none d-lg-inline text-gray-600 small">{this.state.username}</span>
+                                <span class="mr-2 d-lg-inline text-gray-600 small">{this.state.username}</span>
                             </a>
                             {/* <!-- Dropdown - User Information --> */}
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
