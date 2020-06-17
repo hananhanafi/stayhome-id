3bbb13fd1c80f1892e511082210874ce94dc7aba
Hanan Hanafi <hananhanafi76@gmail.com>
2020-06-18 01:16:12 +0700

    backup

--- view/src/components/Footer.js
+++ view/src/components/Footer.js
@@ -4,7 +4,12 @@ function Footer() {
 
 return (
         <div className="footer py-2 text-white" style={{backgroundColor:"#da3150"}}>
-            <h5>Made with <i class="fas fa-heart"></i> by Qomsibe</h5>
+        <h5>Made with <i class="fas fa-heart"></i> by Hanafi</h5>
+          <span>
+            <a href="https://www.instagram.com/hananhnfi"><p className="px-1 text-white" style={{display:"inline"}}>Instagram <i class="fab fa-instagram"></i></p></a>
+            <a href="https://www.linkedin.com/in/hanan-hanafi-702897174"><p className="px-1 text-white" style={{display:"inline"}}>Linkedin <i class="fab fa-linkedin"></i></p></a>
+            <a href="https://twitter.com/han2afi"><p className="px-1 text-white" style={{display:"inline"}}>Twitter <i class="fab fa-twitter"></i></p></a>
+          </span>
         </div>
 )
 }
